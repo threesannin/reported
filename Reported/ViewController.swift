a5cffe1d8c3674524473016505b051011e3944a5
Daniel  Ochoa Aguila <danielochoaaguila@Daniels-Mac.local>
2019-03-27 15:00:47 -0700

    fresh project

---
+++ Reported/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reported
+//
+//  Created by Daniel  Ochoa Aguila on 3/27/19.
+//  Copyright © 2019 Daniel  Ochoa Aguila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
