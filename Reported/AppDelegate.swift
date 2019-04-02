364a2cc150fa4998be26ed0d3d107fb99d40bd7b
Daniel  Ochoa Aguila <danielochoaaguila@Daniels-Mac.local>
2019-04-02 10:20:58 -0700

    Appdelegate file

--- Reported/AppDelegate.swift
+++ Reported/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.initialize(
+            with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "Reported"
+                configuration.server = "https://sannin-reported.herokuapp.com/parse"
+            })
+        )
         return true
     }
 
