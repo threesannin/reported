80bc4d105e28e66a3345b32d7f8398a574cdc641
Jay Arellano <jaarellano@csumb.edu>
2019-04-15 14:16:31 -0700

    added the call to the database pulling the posts

--- Reported/AlertViewController.swift
+++ Reported/AlertViewController.swift
@@ -7,25 +7,58 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
 
 class AlertViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var alertsTableView: UITableView!
+    var posts = [PFObject]()
+    var selectedPost: PFObject!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         alertsTableView.dataSource = self
         alertsTableView.delegate = self
+        alertsTableView.reloadData()
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className: "Post")
+        query.limit = 20
+        
+        query.findObjectsInBackground{ (posts, error) in
+            if posts != nil{
+                self.posts = posts!
+                self.alertsTableView.reloadData()
+            }
+        }
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let post = posts[indexPath.row]
         let cell = alertsTableView.dequeueReusableCell(withIdentifier: "AlertCell") as! AlertCell
         
+        let reportType = post["category"] as! String
+        let reportDesc = post["description"] as! String
+        
+        let finalString = "new " + reportType + " Reported: '" + reportDesc + "'"
+        
+        cell.alertLabel.text = finalString
+        
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        
+        cell.alertImage.af_setImage(withURL: url)
         
         return cell
     }
