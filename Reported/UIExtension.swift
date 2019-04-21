5f4c9a04321bb5a971c42ddb8e67c18e0b46804c
Sampson Liao <sliao@csumb.edu>
2019-04-21 15:57:22 -0700

    reorganized form fields

--- Reported/UIExtension.swift
+++ Reported/UIExtension.swift
@@ -9,6 +9,20 @@
 import UIKit
 import DropDown
 
+@IBDesignable
+class DesignableView: UIView {
+}
+@IBDesignable
+class DesignableButton: UIButton {
+}
+
+@IBDesignable
+class DesignableLabel: UILabel {
+}
+
+@IBDesignable
+class DesignableTextField: UITextField {
+}
 extension UIView{
     func blink() {
         self.alpha = 0.2
@@ -50,6 +64,48 @@ extension UIView{
             }
         }
     }
+    @IBInspectable var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+    
+    @IBInspectable var shadowColor: UIColor? {
+        get {
+            if let color = layer.shadowColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.shadowColor = color.cgColor
+            } else {
+                layer.shadowColor = nil
+            }
+        }
+    }
 }
 
 extension UILabel {
@@ -80,7 +136,7 @@ extension DropDown {
         self.bottomOffset = CGPoint(x: 0, y:(self.anchorView?.plainView.bounds.height)!)
         self.topOffset = CGPoint(x: 0, y:-(self.anchorView?.plainView.bounds.height)!)
         self.dismissMode = .automatic
-        self.shadowOffset=CGSize(width:0 , height: 4)
-        self.shadowRadius=3
+        //self.shadowOffset=CGSize(width:0 , height: 4)
+        //self.shadowRadius=3
     }
 }
