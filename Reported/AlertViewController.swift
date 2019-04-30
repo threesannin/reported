b88090903284cf0241268dcc2f38b10374a417c6
Jay Arellano <jaarellano@csumb.edu>
2019-04-30 08:22:21 -0700

    starting upvote / moving details page to storyboard reference

--- Reported/AlertViewController.swift
+++ Reported/AlertViewController.swift
@@ -15,6 +15,7 @@ class AlertViewController: UIViewController, UITableViewDataSource, UITableViewD
     @IBOutlet weak var alertsTableView: UITableView!
     var posts = [PFObject]()
     var selectedPost: PFObject!
+    var currentLocation: PFGeoPoint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,15 +29,23 @@ class AlertViewController: UIViewController, UITableViewDataSource, UITableViewD
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        let query = PFQuery(className: "Issues")
-        query.limit = 20
-        
-        query.findObjectsInBackground{ (posts, error) in
-            if posts != nil{
-                self.posts = posts!
-                self.alertsTableView.reloadData()
+        PFGeoPoint.geoPointForCurrentLocation{
+            (geoPoint, error) in
+            if error == nil {
+                self.currentLocation = geoPoint
+                print("alerts page currentLocation: ", self.currentLocation)
+                let query = PFQuery(className: "Issues")
+                query.whereKey("location", nearGeoPoint:self.currentLocation, withinMiles: 40.0)
+                query.limit = 15
+                query.findObjectsInBackground{ (posts, error) in
+                    if posts != nil{
+                        self.posts = posts!
+                        self.alertsTableView.reloadData()
+                    }
+                }
             }
         }
+
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
--- Reported/PostDetailsViewController.swift
+++ Reported/PostDetailsViewController.swift
@@ -21,17 +21,19 @@ class PostDetailsViewController: UIViewController {
     @IBOutlet weak var nearestCrossStreetLabel: UILabel!
     @IBOutlet weak var directionOfTravelLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var upvoteLabel: UILabel!
     
     var post: PFObject!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
         
         categoryLabel.text = post["issueCategory"] as? String
         usernameLabel.text = post["username"] as? String
         descriptionLabel.text = post["descripText"] as? String
+        let uvn = post["upVote"] as! Int
+        upvoteLabel.text = String(uvn)
+        
         
         let date = post["issueDateTime"] as? Date
         let dateFormatterPrint = DateFormatter()
@@ -51,15 +53,6 @@ class PostDetailsViewController: UIViewController {
         issueImage.af_setImage(withURL: url)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
